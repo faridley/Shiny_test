bb40ecc807176257eaa11f9f0a96e576cd047f05
Chess Ridley <f.ridley1@newcastle.ac.uk>
2021-04-29 19:10:07 +0100

    Update at the end of synchronous session

--- BasicLayout.R
+++ BasicLayout.R
@@ -3,13 +3,24 @@
 
 library(shiny)
 
+wfarm <- base64enc::dataURI(file="www/UKwindfarm.png", mime="image/png")
+
+
 # Define UI ----
 ui <- fluidPage(
   titlePanel("title panel"),
   
   sidebarLayout(
     sidebarPanel("sidebar panel"),
-    mainPanel("main panel")
+    mainPanel(
+      h1("This is the main heading for my app"),
+      h2("here is a subheading"),
+      p("This website will be to help planners assess potential windfarm
+           development areas in Cumbria, and achieve a ",strong("balance"),
+           "between different ", em("interest groups"), "and other users."
+           )
+      ),
+    position = c("right")
   ) 
 )
 
@@ -20,3 +31,7 @@ server <- function(input, output) {
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
+
+
+
+?sidebarPanel
