788dbe0ee0bebcb8efc44f6bcd864695e4b8be78
Chess Ridley <f.ridley1@newcastle.ac.uk>
2021-04-29 15:24:39 +0100

    New shiny app - first commit

---
+++ BasicLayout.R
@@ -0,0 +1,22 @@
+## Basic_layout ####
+
+
+library(shiny)
+
+# Define UI ----
+ui <- fluidPage(
+  titlePanel("title panel"),
+  
+  sidebarLayout(
+    sidebarPanel("sidebar panel"),
+    mainPanel("main panel")
+  ) 
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
